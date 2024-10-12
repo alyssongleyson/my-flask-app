b7055b3b6c2f491685c068191a4bc71d2a38f48c
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2024-10-12 16:25:31 -0300

    Adding app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# PostgreSQL Settings
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:userpassword@db/mydatabase'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Define the task table template
+class Task(db.Model):
+	id = db.Column(db.Integer, primay_key=True)
+	title = db.Column(db.String(255), nullable=False)
+
+@app.route('/')
+def inde():
+	tasks = Task.query.all()
+	return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+	if request.method == 'POST':
+	title = request.form['title']
+	new_task = Task(title=title)
+	db.session.add(new_task)
+	db.session.commit()
+	return redirect('/')
+
+if __name__ == '__main__':
+	db.create_all() #Create tables in the database
+	app.run(debug=True, host='0.0.0.0')
